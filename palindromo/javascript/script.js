2d9012e1b1ba5850dc3fe9f101e649e5fc73ca82
filippo <schianchi.filippo.93@gmail.com>
2019-10-29 10:59:06 +0100

    fatto esercizio senza join reverse split

--- palindromo/javascript/script.js
+++ palindromo/javascript/script.js
@@ -4,13 +4,30 @@ var parola = prompt("Inserisci la tua parola");
 
 palindroma(parola);
 
+// function palindroma(parola) {
+//     if (parola === parola.split('').reverse().join()) {
+//         document.getElementById("parola").innerHTML = "La parola " + parola + " è palindroma.";
+//         return console.log("parola palindroma");
+//     }
+//     else  {
+//         document.getElementById("parola").innerHTML = "La parola " + parola + " non è palindroma.";
+//         return console.log("parola non palindroma")
+//     }
+// }
+
 function palindroma(parola) {
-    if (parola === parola.split('').reverse().join()) {
-        document.getElementById("parola").innerHTML = "La parola " + parola + " è palindroma.";
-        return console.log("parola palindroma");
+
+    var parolaContrario = "";
+
+    for (var j = parola.length - 1 ; j >= 0; j--) {
+        parolaContrario += (parola[j]);
     }
-    else  {
+
+    if (parolaContrario === parola) {
+        document.getElementById("parola").innerHTML = "La parola " + parola + " è palindroma.";
+        return
+    } else {
         document.getElementById("parola").innerHTML = "La parola " + parola + " non è palindroma.";
-        return console.log("parola non palindroma")
+        return
     }
 }
